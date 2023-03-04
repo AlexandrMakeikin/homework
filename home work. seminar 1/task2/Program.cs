a023c479246f90a088de48d213a4cf1679d32203
LuckyFish <124457679+AlexandrMakeikin@users.noreply.github.com>
2023-03-04 12:28:36 +0300

    Add files via upload

---
+++ home work. seminar 1/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    System.Console.WriteLine($"max= {numberA}; min={numberB}");
+}
+else if (numberA < numberB)
+{
+    System.Console.WriteLine($"max = {numberB}; min = {numberA}");
+}
+else if (numberA == numberB)
+{
+    System.Console.WriteLine($"числа равны ({numberA}={numberB})");
+}
---
+++ home work. seminar 1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.WriteLine("введите число A " );
+int A= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите число B " );
+int B= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите число C " );
+int C= Convert.ToInt32(Console.ReadLine());
+if(A > B && A > C)
+{
+    System.Console.WriteLine($"максимальное число {A}");
+}
+else if (B > A && B > C)
+{
+    System.Console.WriteLine($"максимальное число {B}");
+}
+else if (C > A && C > B)
+{
+    System.Console.WriteLine($"максимальное число {C}");
+}
+
